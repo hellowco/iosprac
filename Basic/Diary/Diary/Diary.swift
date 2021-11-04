b4cdf4c5603e6cb9bef27294885df5d54a00e299
hellowcode <58387211+hellowco@users.noreply.github.com>
2021-11-04 18:04:31 +0900

    TodoList, Diary

---
+++ Basic/Diary/Diary/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 권성은 on 2021/11/04.
+//
+
+import Foundation
+
+struct Diary {
+  var uuidString: String
+  var title: String
+  var contents: String
+  var date: Date
+  var isStar: Bool
+}
---
+++ Basic/Diary/Diary/StarCell.swift
@@ -0,0 +1,21 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 권성은 on 2021/11/04.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.contentView.layer.cornerRadius = 3.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+    }
+}
